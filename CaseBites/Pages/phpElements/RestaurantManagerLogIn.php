20a8e6cf4ed8cec7d9dad8115b05207d60a70256
Shailen Sheth <shailensheth@gmail.com>
2023-03-21 18:52:25 -0400

    New Update
    - Added Navbar
    - Added Seperate log IN Page to choose for customer or restaurant manager
    - Added map react elements to main page (index.html)
    - Added Restaurant Database connection

---
+++ CaseBites/Pages/phpElements/RestaurantManagerLogIn.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "RestaurantUserInfo";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT * FROM `LogInInfo`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<br> id: ". $row["id"]. " - EMAIL: " . $row["emailID"]. "- PASSWORD: ". $row["password"]."<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+
+
+?>
