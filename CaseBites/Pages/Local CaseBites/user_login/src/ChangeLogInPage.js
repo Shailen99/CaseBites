03e422570cf8af1a5039c913de4555d4b22db84a
jimmyngo4 <105337446+jimmyngo4@users.noreply.github.com>
2023-04-27 14:29:33 -0400

    Added images to ChangeLogInPage

--- CaseBites/Pages/Local CaseBites/user_login/src/ChangeLogInPage.js
+++ CaseBites/Pages/Local CaseBites/user_login/src/ChangeLogInPage.js
@@ -4,18 +4,33 @@ const ChangeLogInPage = () => {
   return (
     <div>
       <center>
-        <h1>Log In As A Restaurant or Customer</h1>
+        <h1 style={{ fontSize: "50px" }}>Log In As A Restaurant or Customer</h1>
       </center>
-      <div class="restaurantLogIn">
-        <a href="/reslogin" style={{ fontSize: "30px" }}>
-          {" "}
-          <p>Log in as a restaurant owner</p>
-        </a>
+      <div style={{ alignContent: "left", float: "left", marginLeft: "50px" }}>
+        <div class="restaurantLogIn">
+          <a href="/reslogin" style={{ fontSize: "30px" }}>
+            {" "}
+            <p>Log in as a Restaurant Owner</p>
+            <img
+              src="https://live.staticflickr.com/65535/51061371327_605731f843_b.jpg"
+              alt="..."
+              width="775px"
+            />
+          </a>
+        </div>
       </div>
-      <div class="customerLogIn">
-        <a href="/Login" style={{ fontSize: "30px" }}>
-          <p>Log in as a customer</p>
-        </a>
+      <div style={{ marginTop: "50px" }}>
+        <div class="customerLogIn">
+          <a href="/Login" style={{ fontSize: "30px" }}>
+            <p>Log in as a Customer</p>
+            <img
+              src="https://assets.bonappetit.com/photos/610aa6ddc50e2f9f7c42f7f8/16:9/w_2560%2Cc_limit/Savage-2019-top-50-busy-restaurant.jpg"
+              alt="..."
+              width="750px"
+              height="500px"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
